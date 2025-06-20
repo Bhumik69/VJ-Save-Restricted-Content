7f449ffa94fdf9acc9c17f043b8379fa9c5458e5
Bhumik Dohare <137979644+Bhumik69@users.noreply.github.com>
2025-06-20 12:08:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,16 @@
 import os
 
-# Bot token @Botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-
-# Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", ""))
-
-# Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = "8094935353:AAG_ESPRK21dcWlI0Bw4gLEAl2URnveQqUY"
+API_ID = 23644766
+API_HASH = "9dc15dd41be1a26016b2ebac611868f5"
 
 # Your Owner / Admin Id For Broadcast 
-ADMINS = int(os.environ.get("ADMINS", "6982977861"))
+ADMINS = 6982977861
 
 # Your Mongodb Database Url
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_URI = "mongodb+srv://Group-management:h28hYvrDpnSEcBc8@cluster.2e4dvo1.mongodb.net/groupdb?retryWrites=true&w=majority&tls=true" # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_NAME = os.environ.get("DB_NAME", "groupdb")
+DB_NAME = "groupdb"
 
 # If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
